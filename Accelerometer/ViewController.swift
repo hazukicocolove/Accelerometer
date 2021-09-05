db019165797e8b01ebea35ab9affdc34f4df637a
HazukiTamura <pretty11_13@yahoo.co.jp>
2021-09-06 05:58:27 +0900

    Initial Commit

---
+++ Accelerometer/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Accelerometer
+//
+//  Created by Hazuki Tamura on 2021/09/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func unwindToViewController(segue:UIStoryboardSegue){
+        
+    }
+}
+
